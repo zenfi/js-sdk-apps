ed8f506632b708dd1a31e042670d0584cd282b1e
Manuel de la Torre <manuelmhtr@gmail.com>
2022-06-16 12:52:32 -0500

    v1.3.0: Added Vexi integration

--- src/vexi.js
+++ src/vexi.js
@@ -8,42 +8,52 @@ const zenfi = new ZenfiSDK({
     {
       dataKey: 'email',
       selector: '#email',
+      afterAction: ({ element }) => element.dispatchEvent(new Event('change')),
     },
     {
       dataKey: 'name1',
       selector: '#primer_nombre',
+      afterAction: ({ element }) => element.dispatchEvent(new Event('change')),
     },
     {
       dataKey: 'name2',
       selector: '#segundo_nombre',
+      afterAction: ({ element }) => element.dispatchEvent(new Event('change')),
     },
     {
       dataKey: 'lastName1',
       selector: '#primer_apellido',
+      afterAction: ({ element }) => element.dispatchEvent(new Event('change')),
     },
     {
       dataKey: 'lastName2',
       selector: '#segundo_apellido',
+      afterAction: ({ element }) => element.dispatchEvent(new Event('change')),
     },
     {
       dataKey: 'phone',
       selector: '#celular',
+      afterAction: ({ element }) => element.dispatchEvent(new Event('change')),
     },
     {
       dataKey: 'phone',
       selector: '#confircelular',
+      afterAction: ({ element }) => element.dispatchEvent(new Event('change')),
     },
     {
       dataKey: 'birthdateDay',
       selector: '#dia',
+      afterAction: ({ element }) => element.dispatchEvent(new Event('change')),
     },
     {
       dataKey: 'birthdateMonth',
       selector: '#mes',
+      afterAction: ({ element }) => element.dispatchEvent(new Event('change')),
     },
     {
       dataKey: 'birthdateYear',
       selector: '#anio',
+      afterAction: ({ element }) => element.dispatchEvent(new Event('change')),
     },
     {
       dataKey: 'nationality',
@@ -56,32 +66,67 @@ const zenfi = new ZenfiSDK({
       strategy: 'click',
     },
     {
-      dataKey: 'curp',
-      selector: '#curp',
-    },
-    {
       dataKey: 'rfcPrefix',
       selector: '#rfc',
+      afterAction: ({ element }) => element.dispatchEvent(new Event('change')),
     },
     {
       dataKey: 'rfcSuffix',
       selector: '#homoclave',
+      afterAction: ({ element }) => element.dispatchEvent(new Event('change')),
     },
     {
       dataKey: 'addressStreet',
       selector: '#calle',
+      afterAction: ({ element }) => element.dispatchEvent(new Event('change')),
     },
     {
       dataKey: 'addressExtNumber',
       selector: '#numero_exterior',
+      afterAction: ({ element }) => element.dispatchEvent(new Event('change')),
     },
     {
       dataKey: 'addressIntNumber',
       selector: '#numero_interior',
+      afterAction: ({ element }) => element.dispatchEvent(new Event('change')),
+    },
+    {
+      dataKey: 'addressPostalCode',
+      selector: '#codigo_postal',
+      afterAction: ({ element }) => element.dispatchEvent(new Event('change')),
     },
     {
       dataKey: 'addressNeighborhood',
       selector: '#colonia',
+      afterAction: ({ element }) => element.dispatchEvent(new Event('change')),
+    },
+    {
+      dataKey: 'employmentStatus',
+      selector: 'select#tipo_ingresos',
+      afterAction: ({ element }) => element.dispatchEvent(new Event('change')),
+    },
+    {
+      dataKey: 'income',
+      selector: '#monto_mensual_ingresos',
+      afterAction: ({ element }) => element.dispatchEvent(new Event('change')),
+    },
+    // Leave "birthState" and "curp" at the end
+    {
+      dataKey: 'birthState',
+      selector: '#entidad_nacimiento select',
+      afterAction: ({ element }) => element.dispatchEvent(new Event('change')),
+    },
+    {
+      dataKey: 'curp',
+      selector: '#curp',
+      afterAction: ({ element, value }) => {
+        element.dispatchEvent(new Event('change'));
+        // Set curp again after Vexi overrides it
+        setTimeout(() => {
+          element.value = value;
+          element.dispatchEvent(new Event('change'));
+        }, 2000);
+      },
     },
   ],
 });
@@ -89,6 +134,22 @@ const zenfi = new ZenfiSDK({
 const fillZenfiData = async () => {
   await zenfi.fetchData();
   zenfi.fillTargets();
+
+  // Autofills seconds part of the form, once its visible
+  const continueBtn = document.querySelector('#modalConfirmaDatosPersonales button.btn');
+  if (continueBtn) {
+    continueBtn.addEventListener('click', () => {
+      let interval;
+      const tryFill = () => {
+        const saveBtn = document.querySelector('#btn_guardar');
+        const isVisible = !!saveBtn.offsetParent;
+        if (!isVisible) return;
+        if (interval) clearInterval(interval);
+        zenfi.fillTargets();
+      };
+      interval = setInterval(tryFill, 500);
+    });
+  }
 };
 
 fillZenfiData();
